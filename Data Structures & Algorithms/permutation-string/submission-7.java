483653196cd416b71a0fd8ea0a83b587aa984e77
puja081 <puja.cs08@gmail.com>
2026-04-15 23:18:42 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isMatched(int[] map1, int [] map2) {
+        for(int i = 0; i < 26 ; i++) {
+            if(map1[i] != map2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        if(n < m){
+            return false;
+        }
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+
+        for(int i = 0 ; i < m ; i++) {
+            map1[s1.charAt(i) - 'a']++;
+            map2[s2.charAt(i) - 'a']++;
+        }
+
+        for(int i = 1 ; i <= n-m ; i++){
+            map2[s2.charAt(i-1) - 'a']--;
+            map2[s2.charAt(i+m-1) - 'a']++;
+
+            if(isMatched(map1, map2)) {
+                return true;
+            }
+
+        }
+        return false;
+        
+    }
+
+}
