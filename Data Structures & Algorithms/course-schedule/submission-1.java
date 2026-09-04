39e6cc8e8e9f162dc6c3d64f104e33ce9a5ff8d3
puja081 <puja.cs08@gmail.com>
2026-09-04 14:20:15 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int indegree[] = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] pre : prerequisites){
+             int course = pre[0];
+             int prerequisite = pre[1];
+             indegree[course]++;
+             adj.get(prerequisite).add(course);
+            // indegree[pre[1]]++;
+            // adj.get(pre[0]).add(pre[1]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        int finish = 0;
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            res.add(node);
+            for(int neighbour : adj.get(node)){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0) {
+                    queue.offer(neighbour);
+                }
+            }
+        }
+        if(res.size() == numCourses){
+            return true;
+        }
+        return false;
+        
+    }
+}
