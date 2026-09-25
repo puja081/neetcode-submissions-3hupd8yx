0829c8a66a741092a171c2181ea64a10617d7171
puja081 <puja.cs08@gmail.com>
2026-09-25 19:39:34 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // char[] a = s.toCharArray();
+        // char[] b = t.toCharArray();
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+        // if(Arrays.equals(a,b)) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        int[] count = new int[26]; 
+        for(int i = 0 ; i < s.length() ; i++) {
+                  
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int x : count) {
+            if(x != 0){
+                return false;
+            }
+        }
+        return true;
+        
+
+
+    }
+}
